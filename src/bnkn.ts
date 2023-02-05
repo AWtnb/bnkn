838892ad9c463d4dfc8b30aeefa8850c82385a61
awtnb <arikaebanataw@gmail.com>
2023-02-05 11:56:39 +0900

    fix globally

--- src/bnkn.ts
+++ src/bnkn.ts
@@ -39,7 +39,7 @@ export class Bnkn {
   }
 
   static fixDumbQuotes(s: string): string {
-    return s.replace(/["'].+?["']/, (m: string) => {
+    return s.replace(/["'].+?["']/g, (m: string) => {
       const prefix = m.startsWith('"') ? "\u201c" : "\u2018";
       const suffix = prefix == "\u201c" ? "\u201d" : "\u2019";
       return prefix + m.substring(1, m.length - 1) + suffix;
