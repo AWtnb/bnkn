84bcbe5eb2bb85f83f60224791e332110f3420a5
awtnb <arikaebanataw@gmail.com>
2023-07-24 17:05:40 +0900

    register each bnkn command

--- src/bnkn.ts
+++ src/bnkn.ts
@@ -23,7 +23,7 @@ const toHalfWidthPunc = (s: string): string => {
   return HALFWIDTH_PUNC.charAt(idx);
 };
 
-const formatPunctuationWidth = (s: string): string => {
+export const formatPunctuationWidth = (s: string): string => {
   const stack: string[] = [];
   for (let i = 0; i < s.length; i++) {
     const cur = s.charAt(i);
@@ -54,145 +54,127 @@ const formatPunctuationWidth = (s: string): string => {
   return stack.join("");
 };
 
-export class Bnkn {
-  static toTortoiseBracket(s: string): string {
-    return s.replace(/[（）\(\)]/g, (m: string) => {
-      if (["（", "("].includes(m)) {
-        return "〔";
-      }
-      return "〕";
-    });
-  }
+export const toTortoiseBracket = (s: string): string => {
+  return s.replace(/[（）\(\)]/g, (m: string) => {
+    if (["（", "("].includes(m)) {
+      return "〔";
+    }
+    return "〕";
+  });
+};
 
-  static swapHumanNamePosition(s: string): string {
-    return new HumanName(s).swap();
-  }
+export const swapHumanNamePosition = (s: string): string => {
+  return new HumanName(s).swap();
+};
 
-  static fixDumbQuotes(s: string): string {
-    return s.replace(/["'].+?["']/g, (m: string) => {
-      const prefix = m.startsWith('"') ? "\u201c" : "\u2018";
-      const suffix = prefix == "\u201c" ? "\u201d" : "\u2019";
-      return prefix + m.substring(1, m.length - 1) + suffix;
-    });
-  }
+export const fixDumbQuotes = (s: string): string => {
+  return s.replace(/["'].+?["']/g, (m: string) => {
+    const prefix = m.startsWith('"') ? "\u201c" : "\u2018";
+    const suffix = prefix == "\u201c" ? "\u201d" : "\u2019";
+    return prefix + m.substring(1, m.length - 1) + suffix;
+  });
+};
 
-  static trimBrackets(s: string): string {
-    const lefts = BRACKETS.map((p) => p.charAt(0));
-    const c = s.charAt(0);
-    if (lefts.includes(c)) {
-      const found = BRACKETS.filter((x) => x.charAt(0) == c);
-      if (found.length) {
-        const right = found[0].charAt(1);
-        if (s.endsWith(right)) {
-          return s.substring(1, s.length - 1);
-        }
+export const trimBrackets = (s: string): string => {
+  const lefts = BRACKETS.map((p) => p.charAt(0));
+  const c = s.charAt(0);
+  if (lefts.includes(c)) {
+    const found = BRACKETS.filter((x) => x.charAt(0) == c);
+    if (found.length) {
+      const right = found[0].charAt(1);
+      if (s.endsWith(right)) {
+        return s.substring(1, s.length - 1);
       }
     }
-    return s;
   }
+  return s;
+};
 
-  static toHalfWidth(s: string): string {
-    return s.replace(/[\uff21-\uff3a\uff41-\uff5a\uff10-\uff19]/g, (m: string) => {
-      return String.fromCharCode(m.charCodeAt(0) - 0xfee0);
-    });
-  }
+export const toHalfWidth = (s: string): string => {
+  return s.replace(/[\uff21-\uff3a\uff41-\uff5a\uff10-\uff19]/g, (m: string) => {
+    return String.fromCharCode(m.charCodeAt(0) - 0xfee0);
+  });
+};
 
-  static toFullWidth(s: string): string {
-    return s.replace(/[A-Za-z0-9]/g, (m: string) => {
-      return String.fromCharCode(m.charCodeAt(0) + 0xfee0);
-    });
-  }
+export const toFullWidth = (s: string): string => {
+  return s.replace(/[A-Za-z0-9]/g, (m: string) => {
+    return String.fromCharCode(m.charCodeAt(0) + 0xfee0);
+  });
+};
 
-  static toHalfWidthBracket(s: string): string {
-    return s.replace(/[（）［］]/g, (m: string) => {
-      if (m == "（") return "(";
-      if (m == "）") return ")";
-      if (m == "［") return "[";
-      return "]";
-    });
-  }
+export const toHalfWidthBracket = (s: string): string => {
+  return s.replace(/[（）［］]/g, (m: string) => {
+    if (m == "（") return "(";
+    if (m == "）") return ")";
+    if (m == "［") return "[";
+    return "]";
+  });
+};
 
-  static toFullWidthBracket(s: string): string {
-    return s.replace(/[\(\)\[\]]/g, (m: string) => {
-      if (m == "(") return "（";
-      if (m == ")") return "）";
-      if (m == "[") return "［";
-      return "］";
-    });
-  }
+export const toFullWidthBracket = (s: string): string => {
+  return s.replace(/[\(\)\[\]]/g, (m: string) => {
+    if (m == "(") return "（";
+    if (m == ")") return "）";
+    if (m == "[") return "［";
+    return "］";
+  });
+};
 
-  static toSingle(s: string): string {
-    return s.replace(/[“”『』"]/g, (m: string) => {
-      if (m == "“") return "‘";
-      if (m == "”") return "’";
-      if (m == "『") return "「";
-      if (m == "』") return "」";
-      return "'";
-    });
-  }
+export const toSingle = (s: string): string => {
+  return s.replace(/[“”『』"]/g, (m: string) => {
+    if (m == "“") return "‘";
+    if (m == "”") return "’";
+    if (m == "『") return "「";
+    if (m == "』") return "」";
+    return "'";
+  });
+};
 
-  static toDouble(s: string): string {
-    return s.replace(/[‘’「」']/g, (m: string) => {
-      if (m == "‘") return "“";
-      if (m == "’") return "”";
-      if (m == "「") return "『";
-      if (m == "」") return "』";
-      return '"';
-    });
-  }
+export const toDouble = (s: string): string => {
+  return s.replace(/[‘’「」']/g, (m: string) => {
+    if (m == "‘") return "“";
+    if (m == "’") return "”";
+    if (m == "「") return "『";
+    if (m == "」") return "』";
+    return '"';
+  });
+};
 
-  static formatPunctuation(s: string): string {
-    return formatPunctuationWidth(s)
-      .replace(/[\.,\uff0e\uff0c]./g, (m: string) => {
-        if (m == ".,") {
+export const formatPunctuation = (s: string): string => {
+  return formatPunctuationWidth(s)
+    .replace(/[\.,\uff0e\uff0c]./g, (m: string) => {
+      if (m == ".,") {
+        return m;
+      }
+      const punc = m.charAt(0);
+      const suffix = m.charAt(1);
+      if (punc == "." || punc == ",") {
+        if ([")", "'", '"'].includes(suffix)) {
           return m;
         }
-        const punc = m.charAt(0);
-        const suffix = m.charAt(1);
-        if (punc == "." || punc == ",") {
-          if ([")", "'", '"'].includes(suffix)) {
-            return m;
-          }
-          return suffix == " " ? m : punc + " " + suffix;
-        }
-        return suffix == " " ? m.trim() : m;
-      })
-      .replace(/\.,(?! )/g, "., ");
-  }
-
-  static toggleOxfordComma(s: string): string {
-    if (s.match(/, &/)) {
-      return s.replace(/, &/g, " &");
-    }
-    return s.replace(/ &/g, ", &");
-  }
-
-  static toggleCommaType(s: string): string {
-    return s.replace(/[、\uff0c]/g, (m: string) => {
-      if (m == "\uff0c") {
-        return "、";
+        return suffix == " " ? m : punc + " " + suffix;
       }
-      return "\uff0c";
-    });
-  }
+      return suffix == " " ? m.trim() : m;
+    })
+    .replace(/\.,(?! )/g, "., ");
+};
 
-  static formatHorizontalBars(s: string): string {
-    return s.replace(/(?<=\d)-(?=\d)/g, "\u2013").replace(/(?<=[A-Za-z])-(?=[A-Za-z])/g, "\u2010");
+export const toggleOxfordComma = (s: string): string => {
+  if (s.match(/, &/)) {
+    return s.replace(/, &/g, " &");
   }
-}
+  return s.replace(/ &/g, ", &");
+};
 
-export const BNKN_MENU = new Map();
-BNKN_MENU.set("fix dumb-quotes", Bnkn.fixDumbQuotes);
-BNKN_MENU.set("format horizontal bars", Bnkn.formatHorizontalBars);
-BNKN_MENU.set("format period and comma", Bnkn.formatPunctuation);
-BNKN_MENU.set("swap familyname position", Bnkn.swapHumanNamePosition);
-BNKN_MENU.set("to double-brackets", Bnkn.toDouble);
-BNKN_MENU.set("to full-width", Bnkn.toFullWidth);
-BNKN_MENU.set("to full-width-brackets", Bnkn.toFullWidthBracket);
-BNKN_MENU.set("to half-width", Bnkn.toHalfWidth);
-BNKN_MENU.set("to half-width-brackets", Bnkn.toHalfWidthBracket);
-BNKN_MENU.set("to single-brackets", Bnkn.toSingle);
-BNKN_MENU.set("to to-tortoise-brackets", Bnkn.toTortoiseBracket);
-BNKN_MENU.set("toggle Comma type", Bnkn.toggleCommaType);
-BNKN_MENU.set("toggle Oxford-comma", Bnkn.toggleOxfordComma);
-BNKN_MENU.set("trim brackets", Bnkn.trimBrackets);
+export const toggleCommaType = (s: string): string => {
+  return s.replace(/[、\uff0c]/g, (m: string) => {
+    if (m == "\uff0c") {
+      return "、";
+    }
+    return "\uff0c";
+  });
+};
+
+export const formatHorizontalBars = (s: string): string => {
+  return s.replace(/(?<=\d)-(?=\d)/g, "\u2013").replace(/(?<=[A-Za-z])-(?=[A-Za-z])/g, "\u2010");
+};
