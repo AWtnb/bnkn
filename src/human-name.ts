be25bb4e62a54be8a5a55b10e36669906236dc0a
awtnb <arikaebanataw@gmail.com>
2023-11-15 11:00:52 +0900

    as private as possible

--- src/human-name.ts
+++ src/human-name.ts
@@ -1,43 +1,43 @@
 export class HumanName {
-  readonly origin: string;
-  readonly familyName: string;
-  readonly restName: string;
-  readonly isFamilyNameFirst: boolean;
+  private readonly _origin: string;
+  private readonly _familyName: string;
+  private readonly _restName: string;
+  private readonly _isFamilyNameFirst: boolean;
   constructor(s: string) {
-    this.origin = s.trim();
+    this._origin = s.trim();
     if (s.indexOf(",") != -1) {
-      this.isFamilyNameFirst = true;
-      const [familyName, ...rest] = this.origin.split(",");
-      this.familyName = familyName;
-      this.restName = rest.join("");
+      this._isFamilyNameFirst = true;
+      const [familyName, ...rest] = this._origin.split(",");
+      this._familyName = familyName;
+      this._restName = rest.join("");
       return;
     }
-    this.isFamilyNameFirst = false;
-    const elems = this.origin
+    this._isFamilyNameFirst = false;
+    const elems = this._origin
       .replace(/\./g, ". ")
       .split(" ")
       .filter((x) => x.length);
-    this.restName = elems.filter((x) => x.endsWith(".")).join("");
-    this.familyName = elems.filter((x) => !x.endsWith(".")).join("");
+    this._restName = elems.filter((x) => x.endsWith(".")).join("");
+    this._familyName = elems.filter((x) => !x.endsWith(".")).join("");
   }
 
   private formatFamilyName(): string {
-    return this.familyName.trim();
+    return this._familyName.trim();
   }
 
   private formatRestName(): string {
-    return this.restName.replace(/\./g, ". ").replace(/\s+/g, " ").trim();
+    return this._restName.replace(/\./g, ". ").replace(/\s+/g, " ").trim();
   }
 
   getText(): string {
-    if (this.isFamilyNameFirst) {
+    if (this._isFamilyNameFirst) {
       return this.formatFamilyName() + ", " + this.formatRestName();
     }
     return this.formatRestName() + " " + this.formatFamilyName();
   }
 
   swap(): string {
-    if (this.isFamilyNameFirst) {
+    if (this._isFamilyNameFirst) {
       return this.formatRestName() + " " + this.formatFamilyName();
     }
     return this.formatFamilyName() + ", " + this.formatRestName();
